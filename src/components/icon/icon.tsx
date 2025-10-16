787f1b78d513f7c40a05680732da4eb0d77f29d6
Evgenii Kalkutin <evgenii.kalkutin@gmail.com>
2025-10-16 14:04:41 +0300

    feat(): Add icon component

---
+++ src/components/icon/icon.tsx
@@ -0,0 +1,31 @@
+import clsx from 'clsx';
+import React from 'react';
+
+import styles from './styles.module.css';
+
+type Props = {
+  readonly className?: string;
+
+  readonly size?: number;
+  readonly children: React.ReactElement<{
+    size?: number;
+  }>;
+};
+
+export const Icon: React.FC<Props> = props => {
+  const { size = 24 } = props;
+
+  return (
+    <div
+      className={clsx(styles.wrapper, props.className)}
+      style={{
+        width: size,
+        height: size,
+      }}
+    >
+      {React.cloneElement(props.children, {
+        size: size / 2,
+      })}
+    </div>
+  );
+};
